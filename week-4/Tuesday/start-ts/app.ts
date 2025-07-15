9e387c47b50a20754fb9b334eb5cabfd6445022d
mkabg <m7110645k@gmail.com>
2025-07-15 17:38:39 +0300

    start ts

---
+++ week-4/Tuesday/start-ts/app.ts
@@ -0,0 +1,9 @@
+console.log("hello from ts");
+let str: string = "m";
+let arr: string[] = ["mordechai"];
+console.log(arr[0], str);
+arr.push("7")
+console.log(typeof arr[1]);
+const myName: string = "m"
+console.log(myName);
+
---
+++ week-4/Tuesday/start-ts/myFile.ts
@@ -0,0 +1,9 @@
+enum Role {
+
+}
+
+type Status = "active" | "inactive" | "pending";
+
+function changeStatus(status?: Status) {
+
+}
